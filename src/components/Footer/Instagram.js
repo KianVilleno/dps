6bb3be0d6dbefebe42530cec5f2d5c2a1e1a4165
Kian Villeno <kian@standingby.co>
2021-05-27 12:35:15 +1000

    Added new token for instagram api and fixed the instagram feed

--- src/components/Footer/Instagram.js
+++ src/components/Footer/Instagram.js
@@ -22,9 +22,64 @@ const Instagram = () => {
   }
 
   return (
-    <WrapOuter>
-      <Wrap></Wrap>
-    </WrapOuter>
+    <>
+      <StaticQuery
+        query={graphql`
+          query {
+            allInstaNode(sort: { fields: timestamp, order: DESC }, limit: 20) {
+              nodes {
+                id
+                thumbnails {
+                  src
+                  config_width
+                  config_height
+                }
+                localFile {
+                  childImageSharp {
+                    fixed(height: 500, width: 500) {
+                      srcWebp
+                    }
+                  }
+                }
+              }
+            }
+          }
+        `}
+        render={data => {
+          console.log(data)
+          const images = data.allInstaNode.nodes.map((node, index) => {
+            return (
+              <SlideImage key={`img-${index}`}>
+                <ImageWrap>
+                  <a
+                    href={`https://instagram.com/p/${node.id}`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      src={node.localFile.childImageSharp.fixed.srcWebp}
+                      alt=""
+                    />
+                  </a>
+                </ImageWrap>
+              </SlideImage>
+            )
+          })
+
+          return (
+            <WrapOuter>
+              <Wrap>
+                <Flex flexWrap="wrap">
+                  <SliderStyled>
+                    <Slider {...slideSettings}>{images}</Slider>
+                  </SliderStyled>
+                </Flex>
+              </Wrap>
+            </WrapOuter>
+          )
+        }}
+      />
+    </>
   )
 }
 
